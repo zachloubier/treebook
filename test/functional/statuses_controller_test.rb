3e92baa2fc1330611f3e25525974d828d7101813
Zach Loubier <zach@muvdigital.com>
2013-08-30 10:53:26 -0400

    add status validations and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,7 +23,14 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should create status" do
+    sign_in users(:zach)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,12 +43,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in to edit a status" do
     get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:zach)
+    get :edit, :id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:zach)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status content is at least 2 letters" do
+  	status = Status.new
+  	status.content = "h"
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+  	status = Status.new
+  	# status.user_id = 
+  	status.content = "hello"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
 end
